253380672da4932110874e730fe06d291849ec7d
watchdog1023 <Naickercd@gmail.com>
2019-02-06 14:20:35 +0200

    Add files via upload

---
+++ Matrix Virus.bat
@@ -0,0 +1,14 @@
+@echo off
+color 2
+echo Hello
+echo I am Morpheus
+echo Welcome
+echo virus downloaded
+echo You have now entered The Matrix.....
+Pause
+goto matrix
+:matrix
+color 2
+:start
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
+goto start
